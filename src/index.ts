8253fc8e2f17e55ff66ef2e9e07961a605aef79a
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2025-09-08 10:00:27 -0300

    Tarefa tere finalizada.

--- src/index.ts
+++ src/index.ts
@@ -43,23 +43,7 @@ app.get('/', async (req: Request, res: Response) => {
 });
 
 
-//Tarefa: Criar uma rota get para produtos que retorne a lista de produtos do banco de dados
-//O produto deve ter id, nome preco, urlfoto, descricao
-//Deve-se criar uma tabela no banco de dados AIVEN para armazenar os produtos
-//A resposta deve ser um array de produtos em formato JSON
-//Crie o código sql para criar a tabela de produtos
-/* 
-CREATE TABLE produtos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    preco DECIMAL(10, 2) NOT NULL,
-    urlfoto VARCHAR(255) NOT NULL,
-    descricao TEXT
-);
-Faz pelo menos 3 inserções nessa tabela
-*/ 
-
-
+//Fiz a tarefa e finalizei
 
 
 app.listen(8000, () => {
